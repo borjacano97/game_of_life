bf57a03d6123284d7b85eab2b04666fcb48b481e
Borja Cano <borja@Borja-Portatil.localdomain>
2020-06-01 23:29:09 +0200

    Window visualitation using SDL

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-all:
-	g++ game_of_life.cpp -o game_of_life.exe
-run: game_of_life.exe
+build:
+	g++ game_of_life.cpp -o game_of_life.exe `sdl2-config --cflags --libs`
+run: build
 	./game_of_life.exe
--- game_of_life.cpp
+++ game_of_life.cpp
@@ -3,6 +3,8 @@
 #include <cstring>
 #include <utility>
 #include <unistd.h>
+#include <SDL2/SDL.h>
+
 using uint = unsigned int;
 struct State
 {
@@ -33,21 +35,40 @@ struct State
 
 	inline bool& at(uint row, uint col){ return data[row*cols + col];}
 	inline const bool& at(uint row, uint col) const { return data[row*cols + col];}
-
+	inline static void swap(State& a, State& b)
+	{
+		auto aux = a.data;
+		a.data = b.data;
+		b.data = aux;
+	}
 	inline uint aliveNeightbours(uint row, uint col) const
 	{
-		uint neightbours = 0;
-		for(int i = -1; i <= 1; ++i)
+		// uint neightbours = 0;
+		// for(int i = -1; i <= 1; ++i)
+		// {
+		// 	for(int j = -1; j <= 1; ++j)
+		// 	{
+		// 		uint index =
+		// 					(row + rows + i) % rows * cols +
+		// 					(col + cols + j) % cols;
+		// 		neightbours += data[index];
+		// 	}
+		// }
+		// return neightbours;
+		uint n = 0;
+		int r, c;
+		for(int i = -1; i <=1; i++)
 		{
-			for(int j = -1; j <= 1; ++j)
+			for(int j = -1; j <= 1; j++)
 			{
-				uint index =
-							(row + rows + i) % rows * cols +
-							(col + cols + j) % cols;
-				neightbours += data[index];
+				r = row + i;
+				c = col + j;
+				r = r < 0 ? rows - 1 : r == rows - 1 ? 0 : r;
+				c = c < 0 ? cols - 1 : c == cols - 1 ? 0 : c;
+				n += at(r, c);
 			}
 		}
-		return neightbours;
+		return n;
 	}
 	inline void print() const
 	{
@@ -70,7 +91,57 @@ struct State
 		}
 	}
 };
+struct Window
+{
+	uint width, height, pixel_size;
+	SDL_Window* window;
+	SDL_Renderer* renderer;
+	static bool initialized;
+	Window(uint width, uint height, uint pixel_size):
+		width(width), height(height), pixel_size(pixel_size)
+	{
+		if(!initialized)
+		{
+			initialized = true;
+			SDL_Init(SDL_INIT_VIDEO);
+		}
+		SDL_CreateWindowAndRenderer(width, height, 0, &window, &renderer);
+	}
+	~Window()
+	{
+		SDL_DestroyRenderer(renderer);
+		SDL_DestroyWindow(window);
+	}
+	void PrintState(const State& state)
+	{
+		SDL_SetRenderDrawColor(renderer, 0,0,0,255);
+		SDL_RenderClear(renderer);
+		for(uint row = 0; row < state.rows; ++row)
+		{
+			for(uint col=0; col < state.cols; ++col)
+			{
+				state.at(row, col) ?
+					SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255):
+					SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+				// float r = row / float(state.rows);
+				// float g = col / float(state.cols);
+				// float b = 0.5f;
 
+				// SDL_SetRenderDrawColor(renderer, Uint8(r*255), Uint8(g*255), Uint8(b*255), Uint8(255));
+				SDL_Rect pixel;
+				pixel.x = col * pixel_size;
+				pixel.y = row * pixel_size;
+				pixel.h = pixel_size;
+				pixel.w = pixel_size;
+				//SDL_FillRect(renderer, &pixel);
+				SDL_RenderFillRect(renderer, &pixel);
+			}
+		}
+		SDL_RenderPresent(renderer);
+	}
+
+};
+bool Window::initialized = false;
 //1. Any live cell with fewer than two live neighbours dies, as if by underpopulation.
 //2. Any live cell with two or three live neighbours lives on to the next generation.
 //3. Any live cell with more than three live neighbours dies, as if by overpopulation.
@@ -81,38 +152,66 @@ bool apply_rules(bool alive, uint neightbours)
 		?	neightbours == 2 || neightbours == 3
 		:	neightbours == 3;
 }
-
+void random_populate(State& state)
+{
+	for(uint i = 0; i< state.rows; i++)
+		for(uint j = 0; j < state.cols; j++)
+			state.at(i, j) = rand() & 1;
+}
+void handle_events(bool& close, State& state)
+{
+	SDL_Event e;
+	while(SDL_PollEvent(&e))
+	{
+		if(e.type == SDL_QUIT)
+		{
+			SDL_Quit();
+			close = true;
+		}
+		else if(e.type == SDL_KEYDOWN && e.key.keysym.sym == SDLK_SPACE)
+		{
+			random_populate(state);
+		}
+	}
+}
+void sim_step(State& state, State& next_state)
+{
+	for(uint i = 0; i < state.rows; ++i)
+	{
+		for(uint j = 0; j < state.cols; ++j)
+		{
+			next_state(i, j) = apply_rules(state(i, j), state.aliveNeightbours(i, j));
+		}
+	}
+}
 int main(int argc, char const *argv[])
 {
-	uint rows = 10, cols= 10;
+	uint rows = 100, cols= 100;
 	if(argc == 3)
 	{
 		rows = atoi(argv[1]);
 		cols = atoi(argv[2]);
 	}
 
-	State state0(rows, cols);
-	State state1(rows, cols);
-	State*		state = &state0;
-	State* next_state = &state1;
-
-	for(uint i = 0; i < rows; ++i)
-		for (uint j = 0; j < cols; j++)
-			state->at(i, j) = bool(rand() & 1);
+	State state(rows, cols);
+	State next_state(rows, cols);
+	random_populate(state);
 	uint gen = 0;
-	while (1)
+	bool close = false;
+	uint win_width = 500;
+	uint win_height = 500;
+	uint pixel_size = win_width / cols;
+	Window window(win_width, win_height, pixel_size);
+	while (!close)
 	{
-		printf("Generation %u\n", gen++);
-		for(uint i = 0; i < rows; ++i)
-		{
-			for(uint j = 0; j < cols; ++j)
-			{
-				next_state->at(i, j) = apply_rules(state->at(i, j), state->aliveNeightbours(i, j));
-			}
-		}
-		state->print();
-		sleep(3);
-		std::swap(state, next_state);
+		handle_events(close, state);
+		sim_step(state, next_state);
+		window.PrintState(state);
+		printf("Generation: %d\r", gen++);
+		SDL_Delay(10000);
+		
+		//swap states
+		State::swap(state, next_state);
 	}
 	return 0;
 }
